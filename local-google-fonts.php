eeac185777a166de99002b17762445cb408bd1eb
Xaver <xaver@everpress.co>
2022-05-27 16:15:19 +0200

    -   improved handling of fonts -   only loading required font files -   supports now different types of variant declarations -   add variants to list only if available in the source -   added some css to improve settings page -   you may have to reload fonts so please check the settings page

--- local-google-fonts.php
+++ local-google-fonts.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Local Google Fonts
 Description: Host your used Google fonts on your server and make your site GDPR compliant.
-Version: 0.3
+Version: 0.4
 Author: EverPress
 Author URI: https://everpress.co
 Text Domain: local-google-fonts
